4098073941576bed2e3b50fee8562299b1def7f2
Ave1Cado <artekjey173@gmail.com>
2024-03-21 12:27:52 +0300

    master

---
+++ Lab4DS/Lab4DS/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Lab4DS.bibliotekaDataSetTableAdapters;
+
+namespace Lab4DS
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BooksTableAdapter books = new BooksTableAdapter();
+        AuthorsTableAdapter authors = new AuthorsTableAdapter();
+        public MainWindow()
+        {
+            InitializeComponent();
+            BooksDrg.ItemsSource = books.GetData();
+            AuthorsCbx.ItemsSource = authors.GetData();
+            BooksDrg.DisplayMemberPath = "Name";
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            BooksDrg.ItemsSource = books.GetDataBy(SearchTxt.Text);
+        }
+
+      
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            BooksDrg.ItemsSource = books.GetData();
+        }
+
+        private void AuthorsCbx_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (AuthorsCbx.SelectedItem != null)
+            {
+                var id = (int)(AuthorsCbx.SelectedItem as DataRowView)?.Row[0];
+                BooksDrg.ItemsSource = books.GetDataBy1(id);
+            }
+
+        }
+    }
+}
---
+++ Lab4EF/Lab4EF/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab4EF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bibliotekaEntities context = new bibliotekaEntities();
+        public MainWindow()
+        {
+            InitializeComponent();
+            BooksDgr.ItemsSource = context.Books.ToList();
+            AuthorsCbx.ItemsSource = context.Authors.ToList();
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+           BooksDgr.ItemsSource = context.Books.ToList().Where(item => item.Title.Contains(SearchTxt.Text));
+        }
+
+        private void AuthorsCbx_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (AuthorsCbx.SelectedItem != null)
+            {
+                var selected = AuthorsCbx.SelectedItem as Authors;
+                BooksDgr.ItemsSource = context.Books.ToList().Where(item => item.Authors == selected);
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            BooksDgr.ItemsSource = context.Books.ToList();
+        }
+    }
+}
